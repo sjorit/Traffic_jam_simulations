e63231d4f035a20299fc470954015819d760acaf
sjorit <sjorit@ethz.ch>
2015-12-01 21:54:06 +0100

    fixed big-gap-jam bug

--- code/core.m
+++ code/core.m
@@ -14,6 +14,9 @@ function core()
     for i = (0.05*number_iterations):number_iterations
         road = update_traffic(road, v_unit, p_toddle_slow, p_toddle_fast, v_max, v_min, v_slow_fast_differenciation);
         r_plot_data = prepare_road_for_plot(road);
+        if i > 100 
+            road
+        end
         plot((0:length(road)-1), r_plot_data*i, '. r')%',MarkerFaceColor', 'r', 'MarkerEdgeColor', 'w')
         %y=0 voll weil alle die möglichen ohne autos auf x achse geplottet
         %werden
