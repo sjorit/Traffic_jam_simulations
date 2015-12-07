19215abaf831302c385e99069a4696d385cbfe0b
sjorit <sjorit@ethz.ch>
2015-12-07 17:26:46 +0100

    state before change to lit.flow rate

--- code/plot_stuff.m
+++ code/plot_stuff.m
@@ -2,25 +2,25 @@ clear;
 label_list={};
 x_data = [];
 y_data = [];
-for v = 0:100
+for v = 0:10
     %p_occupation = v / 100;
-    p_toddle_slow = v/100;
-    %v_max = 20. + v;
-    x_param = p_toddle_slow;
+    %p_toddle_slow = v/100;
+    v_max = 20. + v*8;
+    x_param = v_max;
     [res_tot, road, density_of_cars] = core(x_param);
     hold on
     x_data = [x_data, x_param];
     %x_data = [x_data, density_of_cars];
     y_data = [y_data, mean(res_tot)];
-    %plot(linspace(0, length(res_tot), length(res_tot)), res_tot/length(res_tot)) %label = xparam
-    %label_list = [label_list, num2str(v_max)];
+    plot(linspace(0, length(res_tot), length(res_tot)), res_tot) %label = xparam
+    label_list = [label_list, num2str(v_max)];
 end
 
-plot(x_data, y_data, '-x')
+%plot(x_data, y_data, '-x')
 
 ylim([0,1])
-xlim([0,1])
+xlim([0,500])
 ylabel('average flow rate')
-xlabel('probability of toddling for slow cars')
+xlabel('time step')
 label_list = strcat('v_{max} = ', label_list, ' m/s');
-%legend(label_list)
+legend(label_list)
--- code/user_input.m
+++ code/user_input.m
@@ -3,10 +3,10 @@
 rng(5);
 p_occupation = 0.3;
 initial_obstacle = false;
-%p_toddle_slow = 0.3;
+p_toddle_slow = 0.3;
 p_toddle_fast = p_toddle_slow * 1;
 road_length = 700;
-v_max = 80; %maximum allowed velocity of a car
+%v_max = 80; %maximum allowed velocity of a car
 v_min = 15; %minimum initial velocity of a car
 v_unit = 27/3.6; %m/s velocity unit (one cell of the road)
 v_slow_fast_differenciation = 0.35 * v_max; %ca 100km/h bei v_max = 80 m/s
